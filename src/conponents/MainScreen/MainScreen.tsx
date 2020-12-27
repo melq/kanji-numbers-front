277f0c7343d1a819f63c2c6d595c5bcde36f57ca
melq <tu2sice@yahoo.co.jp>
2020-12-27 12:47:09 +0900

    :construction: HelloWorld した

---
+++ src/conponents/MainScreen/MainScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MainWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`
+
+const MainScreen: React.FC = () => {
+  return (
+    <MainWrapper>
+      Hello.
+    </MainWrapper>
+  )
+}
+
+export default MainScreen
---
+++ src/containers/MainScreenConteiner.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MainScreen from "../conponents/MainScreen/MainScreen";
+
+const MainScreenContainer: React.FC = () => {
+  return <MainScreen />
+}
+
+export default MainScreenContainer
